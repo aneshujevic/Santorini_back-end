30d422a699ddc13ddaed766e90450e3cd0c669cb
owlhoo <anes1996_h@hotmail.com>
2020-01-11 23:50:10 +0100

    updated some stuff

--- santorini/serializers.py
+++ santorini/serializers.py
@@ -16,12 +16,10 @@ def serialize_move(move):
 # valid moves or builds
 def serialize_valid_moves(moves):
 	"""
-	:param moves: list of moves (be it a builds or moving moves)
+	:param moves: dictionary of moves (be it a builds or moving moves)
 	:return: JSON string which contains all available moves with appropriate number in sequence
 	"""
-	length = len(moves)
-	dict_to_serialize = {i: moves[i] for i in range(length)}
-	return json.dumps(dict_to_serialize)
+	return json.dumps(moves)
 
 
 # deserializing incoming JSON request carrying information about ongoing game
@@ -32,7 +30,7 @@ def deserialize_valid_moves_request(data_json):
 	"""
 	decoded_info = json.loads(data_json)[0]
 	board_array = decoded_info["cells"]
-	board_matrix = [[board_array[i + j] for i in range(5)] for j in range(5)]
+	board_matrix = [[board_array[i * 5 + j] for i in range(5)] for j in range(5)]
 	board = Board(board_matrix)
 	builders_coordinates = [
 		decoded_info["firstHE"], decoded_info["secondHE"],
@@ -41,12 +39,12 @@ def deserialize_valid_moves_request(data_json):
 	# TODO: proveriti ID-eve od buildera zbog minimaxa
 	# first two builders are AI
 	# second two builders are HU
-	for i in range(1):
+	for i in range(4):
 		if i < 2:
 			affiliation = "AI"
 		else:
 			affiliation = "HU"
-		new_builder = board.add_builder(affiliation, builders_coordinates[i], -(i + 1))
+		new_builder = board.add_builder(affiliation, builders_coordinates[i], - (i + 1))
 		builder_x = builders_coordinates[i][0]
 		builder_y = builders_coordinates[i][1]
 		previous_value_of_cell = (board.board_state[builder_x][builder_y] + 1) % 5
--- santorini/views.py
+++ santorini/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from santorini.santorini_models.board import Board
 
 from .serializers import serialize_move, serialize_valid_moves, deserialize_valid_moves_request
 
@@ -8,13 +9,19 @@ from .serializers import serialize_move, serialize_valid_moves, deserialize_vali
 @csrf_exempt
 def available_moves(request):
     start_position, board = deserialize_valid_moves_request(request.body)
+    all_avail_moves = board.get_all_available_moves()
+    list_of_valid_moves = {"moves": Board.get_valid_moving_moves(start_position, board, all_avail_moves)}
+    return HttpResponse(serialize_valid_moves(list_of_valid_moves))
+
+@csrf_exempt
+def available_builds(request):
+    start_position, board = deserialize_valid_moves_request(request.body)
+    all_avail_moves = board.get_all_available_moves()
+    list_of_valid_builds = {"moves": Board.get_valid_builds(start_position, all_avail_moves)}
     print(start_position)
     print(board)
-    return HttpResponse("")
-
-
-def available_builds(request):
-    return None
+    print(list_of_valid_builds)
+    return HttpResponse(serialize_valid_moves(list_of_valid_builds))
 
 
 def minimax(request):
